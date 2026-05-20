5bdc3da11787a39ae025347ca3c5a5b399dc3e49
Oscar Munoz <omunoz@visbl.com>
2026-05-20 10:20:00 -0500

    improvements into the case study cpt

--- src/wp-content/plugins/case-studies-cpt/case-studies-cpt.php
+++ src/wp-content/plugins/case-studies-cpt/case-studies-cpt.php
@@ -12,61 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function custom_register_case_studies_and_taxonomies() {
 
-    // 1. Registrar Taxonomía Jerárquica: Industry
-    $labels_industry = array(
-        'name'              => _x( 'Industries', 'taxonomy general name', 'textdomain' ),
-        'singular_name'     => _x( 'Industry', 'taxonomy singular name', 'textdomain' ),
-        'search_items'      => __( 'Search Industries', 'textdomain' ),
-        'all_items'         => __( 'All Industries', 'textdomain' ),
-        'parent_item'       => __( 'Parent Industry', 'textdomain' ),
-        'parent_item_colon' => __( 'Parent Industry:', 'textdomain' ),
-        'edit_item'         => __( 'Edit Industry', 'textdomain' ),
-        'update_item'       => __( 'Update Industry', 'textdomain' ),
-        'add_new_item'      => __( 'Add New Industry', 'textdomain' ),
-        'new_item_name'     => __( 'New Industry Name', 'textdomain' ),
-        'menu_name'         => __( 'Industry', 'textdomain' ),
-    );
-
-    $args_industry = array(
-        'hierarchical'      => true,
-        'labels'            => $labels_industry,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'industry' ),
-        'show_in_rest'      => true, // Habilita Gutenberg
-    );
-
-    register_taxonomy( 'industry', array( 'case_study' ), $args_industry );
-
-    // 2. Registrar Taxonomía Jerárquica: Technology
-    $labels_technology = array(
-        'name'              => _x( 'Technologies', 'taxonomy general name', 'textdomain' ),
-        'singular_name'     => _x( 'Technology', 'taxonomy singular name', 'textdomain' ),
-        'search_items'      => __( 'Search Technologies', 'textdomain' ),
-        'all_items'         => __( 'All Technologies', 'textdomain' ),
-        'parent_item'       => __( 'Parent Technology', 'textdomain' ),
-        'parent_item_colon' => __( 'Parent Technology:', 'textdomain' ),
-        'edit_item'         => __( 'Edit Technology', 'textdomain' ),
-        'update_item'       => __( 'Update Technology', 'textdomain' ),
-        'add_new_item'      => __( 'Add New Technology', 'textdomain' ),
-        'new_item_name'     => __( 'New Technology Name', 'textdomain' ),
-        'menu_name'         => __( 'Technology', 'textdomain' ),
-    );
-
-    $args_technology = array(
-        'hierarchical'      => true,
-        'labels'            => $labels_technology,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'technology' ),
-        'show_in_rest'      => true, // Habilita Gutenberg
-    );
-
-    register_taxonomy( 'technology', array( 'case_study' ), $args_technology );
-
-    // 3. Registrar el Custom Post Type: Case Study
+    // 1. Registrar el Custom Post Type: Case Study
     $labels_cpt = array(
         'name'                  => _x( 'Case Studies', 'Post type general name', 'textdomain' ),
         'singular_name'         => _x( 'Case Study', 'Post type singular name', 'textdomain' ),
@@ -102,6 +48,66 @@ function custom_register_case_studies_and_taxonomies() {
     );
 
     register_post_type( 'case_study', $args_cpt );
+
+    // 2. Registrar Taxonomía Jerárquica: Industry
+    $labels_industry = array(
+        'name'              => _x( 'Industries', 'taxonomy general name', 'textdomain' ),
+        'singular_name'     => _x( 'Industry', 'taxonomy singular name', 'textdomain' ),
+        'search_items'      => __( 'Search Industries', 'textdomain' ),
+        'all_items'         => __( 'All Industries', 'textdomain' ),
+        'parent_item'       => __( 'Parent Industry', 'textdomain' ),
+        'parent_item_colon' => __( 'Parent Industry:', 'textdomain' ),
+        'edit_item'         => __( 'Edit Industry', 'textdomain' ),
+        'update_item'       => __( 'Update Industry', 'textdomain' ),
+        'add_new_item'      => __( 'Add New Industry', 'textdomain' ),
+        'new_item_name'     => __( 'New Industry Name', 'textdomain' ),
+        'menu_name'         => __( 'Industry', 'textdomain' ),
+    );
+
+    $args_industry = array(
+        'hierarchical'      => true,
+        'labels'            => $labels_industry,
+        'public'            => true,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'industry' ),
+        'show_in_rest'      => true, // Habilita Gutenberg
+    );
+
+    register_taxonomy( 'industry', array( 'case_study' ), $args_industry );
+
+    // 3. Registrar Taxonomía Jerárquica: Technology
+    $labels_technology = array(
+        'name'              => _x( 'Technologies', 'taxonomy general name', 'textdomain' ),
+        'singular_name'     => _x( 'Technology', 'taxonomy singular name', 'textdomain' ),
+        'search_items'      => __( 'Search Technologies', 'textdomain' ),
+        'all_items'         => __( 'All Technologies', 'textdomain' ),
+        'parent_item'       => __( 'Parent Technology', 'textdomain' ),
+        'parent_item_colon' => __( 'Parent Technology:', 'textdomain' ),
+        'edit_item'         => __( 'Edit Technology', 'textdomain' ),
+        'update_item'       => __( 'Update Technology', 'textdomain' ),
+        'add_new_item'      => __( 'Add New Technology', 'textdomain' ),
+        'new_item_name'     => __( 'New Technology Name', 'textdomain' ),
+        'menu_name'         => __( 'Technology', 'textdomain' ),
+    );
+
+    $args_technology = array(
+        'hierarchical'      => true,
+        'labels'            => $labels_technology,
+        'public'            => true,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'technology' ),
+        'show_in_rest'      => true, // Habilita Gutenberg
+    );
+
+    register_taxonomy( 'technology', array( 'case_study' ), $args_technology );
+
+    // Explicitly bind taxonomies to post type for Gutenberg REST API compatibility
+    register_taxonomy_for_object_type( 'industry', 'case_study' );
+    register_taxonomy_for_object_type( 'technology', 'case_study' );
 }
 
 add_action( 'init', 'custom_register_case_studies_and_taxonomies' );
